7a8043d7654d93674afdf6656b8017b4d84c6017
Faxcundo <fsalaberry505@gmail.com>
2025-09-27 22:25:09 -0300

    Mas fixs

--- src/main/java/iiss/api_culturarte/ApiCulturarteApplication.java
+++ src/main/java/iiss/api_culturarte/ApiCulturarteApplication.java
@@ -2,12 +2,19 @@ package iiss.api_culturarte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "iiss.api_culturarte",
+    "Controller",
+    "Service",
+    "Repositorios"
+})
 public class ApiCulturarteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiCulturarteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCulturarteApplication.class, args);
+    }
 
 }
